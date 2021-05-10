2c95739ed79d147c864bab9e7a1922d32c572435
chanqun <cksgns93@naver.com>
2021-05-10 21:24:58 +0900

    spring initilizr, 4장 정리 시작

---
+++ kotlinStudy/src/test/kotlin/com/example/kotlin/HelloTest.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+internal class HelloTest {
+
+    @Test
+    @DisplayName("live template test")
+    fun hello() {
+
+    }
+}
